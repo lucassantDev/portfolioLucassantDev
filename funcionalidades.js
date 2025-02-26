fda94f8c58bb9389bc70f30a0e7604703a0b345f
Lucas Sant <lucasfdesantana095@gmail.com>
2025-02-25 22:40:20 -0300

    incluindo funcionnalidades com javascript - 2025-02-25

---
+++ funcionalidades.js
@@ -0,0 +1,10 @@
+addEventListener("click", githubLink());
+addEventListener("click", linkedinLink());
+
+function githubLink(){
+    window.open("https://github.com/lucassantDev");
+}
+
+function linkedinLink(){
+    window.open("https://www.linkedin.com/in/lucasferreira2023--/");
+}
