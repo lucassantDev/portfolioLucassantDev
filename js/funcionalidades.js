ed5140f50bafbc43741767f323ddb868cc1bd599
Lucas Santana <lucasfdesantana095@gmail.com>
2025-03-10 19:54:22 -0300

    adicionando funcionalidades ao mobile(js) - 10-03-2025

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,7 +1,5 @@
 addEventListener("click", githubLink());
 addEventListener("click", linkedinLink());
-addEventListener("click", mudarCor());
-
 
 function githubLink(){
     window.open("https://github.com/lucassantDev");
@@ -11,10 +9,18 @@ function linkedinLink(){
     window.open("https://www.linkedin.com/in/lucasferreira2023--/");
 }
 
-
+addEventListener("click", mudarCor());
 function mudarCor(){
     let modoTela = document.getElementById('modoTela');
     let body = document.querySelector('body');
     modoTela.classList.toggle('dark'); // a propriedade toggle insere ou remove uma classe 
     body.classList.toggle('dark'); // a propriedade toggle insere ou remove uma classe 
+}
+
+addEventListener("click", mudarCorMobile());
+function mudarCorMobile(){
+    let modoDarkLight = document.getElementById('modoDarkLight');
+    let body = document.querySelector('body');
+    modoDarkLight.classList.toggle('darkMobile'); // a propriedade toggle insere ou remove uma classe 
+    body.classList.toggle('darkMobile'); // a propriedade toggle insere ou remove uma classe 
 }
